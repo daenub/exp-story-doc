ef2c12489fac2b462ca27fdbb20fc35e886c04fc
Dan Büschlen <danbueschlen@gmail.com>
2019-10-25 23:42:54 +0200

    hardcode center of slides. calculate the position of the center of the slides on the slides of the canvas. add debug indicators

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -4,6 +4,8 @@ export function initSlideshows() {
   $$("[data-slideshow]").forEach(el => new Slideshow(el))
 }
 
+const DEBUG = false
+
 const parseCoordinates = coordString => {
   const cords = coordString.split("x")
 
@@ -23,8 +25,6 @@ class Slideshow {
     this.image = $("[data-image]", this.el)
     this.button = $("[data-control]", this.el)
 
-    console.log(this.canvas.getBoundingClientRect())
-
     this.currentSlide = 0
 
     this.init()
@@ -39,10 +39,21 @@ class Slideshow {
     if (this.slides.length) {
       this.revealSlide()
     }
+
+    // Debug indicators
+    if (DEBUG) {
+      let div
+
+      for (let i = 0; i < 4; i++) {
+        div = document.createElement("div")
+        div.classList.add("debug")
+        div.classList.add("debug--" + (i + 1))
+        this.canvas.appendChild(div)
+      }
+    }
   }
 
   revealSlide(index = null) {
-    console.info("revealSlide")
     if (index !== null) {
       this.currentSlide = index
     }
@@ -50,30 +61,32 @@ class Slideshow {
   }
 
   nextSlide() {
-    console.info("nextSlide")
     this.revealSlide(this.currentSlide + 1)
   }
 
   mapCoordsToCurrentSize({x, y}) {
     const canvasRect = this.canvas.getBoundingClientRect()
 
-    // TODO: fix the original coords
-    x = x + (788 - 394)
-    y = y + (799 - 396)
-
     return {
       x: map(x, 0, this.viewBox.x, 0, canvasRect.width),
-      y: map(y, 0, this.viewBox.y, 0, canvasRect.height),
+      y: map(y, 0, this.viewBox.y, 0, canvasRect.height)
     }
   }
 
   getCenterPosition({x, y}) {
+    const canvasRect = this.canvas.getBoundingClientRect()
 
+    return {
+      x: (canvasRect.width / 2) - x,
+      y: (canvasRect.height / 2) - y,
+    }
   }
 
   applyTransform(coords) {
     const scaledCoords = this.mapCoordsToCurrentSize(coords)
-    console.log(`translate(${scaledCoords.x}px, ${scaledCoords.y}px)`)
-    this.image.style.transform = `translate(${-1 * scaledCoords.x}px, ${-1 * scaledCoords.y}px)`
+    const centeredCords = this.getCenterPosition(scaledCoords)
+
+    this.image.style.transform = `translate(${centeredCords.x}px, ${centeredCords.y}px) scale(2.5)`
+    this.image.style.transformOrigin = `${scaledCoords.x}px ${scaledCoords.y}px`
   }
 }
